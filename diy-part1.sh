685f99e0b8d9330840d4ecf88d80447b37ebee59
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2021-03-28 16:10:42 +0800

    Initial commit

---
+++ diy-part1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/281677160/openwrt-package' feeds.conf.default
+#添加openclash
+sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git' feeds.conf.default
+#添加password
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+#新的argon主题
+rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#使用开源驱动
+rm -rf ./package/lean/mt
