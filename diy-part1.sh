68662f72da7c4040eefd146a59c8e44d1f05a669
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2021-04-15 17:02:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,10 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/281677160/openwrt-package' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #添加openclash
 sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git' feeds.conf.default
 #添加password
@@ -22,3 +19,8 @@ sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' f
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 #使用开源驱动
 rm -rf ./package/lean/mt
+#删除有问题的netdata
+rm -rf ./package/lean/luci-app-netdata
+# Add a feed source
+sed -i '$a src-git lienol https://github.com/281677160/openwrt-package' feeds.conf.default
+
