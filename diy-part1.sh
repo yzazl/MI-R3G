0b9e9e59c619114857e931ef0a235230ae889690
yzazl <77037913+yzazl@users.noreply.github.com>
2021-04-28 11:35:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #添加openclash
 sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git' feeds.conf.default
 #添加password
